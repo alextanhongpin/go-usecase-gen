1c666d539364b8426a10e135e527ae33a8eaf761
alextanhongpin <alextan220990@gmail.com>
2020-10-26 00:02:48 +0800

    chore: update struct

--- README.md
+++ README.md
@@ -4,4 +4,13 @@ create a new blog account
 | Title | Description |
 | -     | -           |
 | Name | create a new blog account |
-| Related requirements | 1. Requirement A.1<br/>2. Requirement A.2|
+| Related requirements | Requirement A.1<br/>Requirement A.2|
+| Goal in Context | A new existing author requests a new blog account from the Administrator|
+| Preconditions | The author has appropriate proof of identity.|
+| Successful end conditions | a new blog account is created for the author.|
+| Failed end conditions | The application for a new blog account is rejected.|
+| Primary Actors | Administrator|
+| Secondary Actors | Author Credential Database|
+| Trigger | The administrator asks the CMS to create a new blog account|
+| Main flow | 1. The Administrator asks the system to create a new blog<br/>2. The Administrator selects an account type.<br/>3. The Administrator enters the author's details.<br/>4. The author's details are verified using the Author Credential Database.<br/>5. The new blog account is created.<br/>6. A summary of the new blog account's details are emailed to the author.|
+| Extensions | 4.1. The Author Credentials Database does not verify the Author's details.<br/>4.2. The author's blog account application is rejected.|
--- main.go
+++ main.go
@@ -12,9 +12,18 @@ import (
 )
 
 type UseCase struct {
-	Version             string
-	Name                string   `yaml:"name"`
-	RelatedRequirements []string `yaml:"related_requirements"`
+	Version                 string
+	Name                    string   `yaml:"name"`
+	RelatedRequirements     []string `yaml:"related_requirements"`
+	GoalInContext           []string `yaml:"goal_in_context"`
+	Preconditions           []string `yaml:"preconditions"`
+	SuccessfulEndConditions []string `yaml:"successful_end_conditions"`
+	FailedEndConditions     []string `yaml:"failed_end_conditions"`
+	PrimaryActors           []string `yaml:"primary_actors"`
+	SecondaryActors         []string `yaml:"secondary_actors"`
+	Trigger                 []string `yaml:"trigger"`
+	MainFlow                []string `yaml:"main_flow"`
+	Extensions              []string `yaml:"extensions"`
 }
 
 const readme = `
@@ -24,6 +33,15 @@ const readme = `
 | -     | -           |
 | Name | {{ .Name }} |
 | Related requirements | {{ list .RelatedRequirements }}|
+| Goal in Context | {{ list .GoalInContext }}|
+| Preconditions | {{ list .Preconditions }}|
+| Successful end conditions | {{ list .SuccessfulEndConditions }}|
+| Failed end conditions | {{ list .FailedEndConditions }}|
+| Primary Actors | {{ list .PrimaryActors }}|
+| Secondary Actors | {{ list .SecondaryActors }}|
+| Trigger | {{ list .Trigger }}|
+| Main flow | {{ list .MainFlow }}|
+| Extensions | {{ list .Extensions }}|
 `
 
 func main() {
@@ -41,11 +59,11 @@ func main() {
 			if len(str) == 1 {
 				return template.HTML(str[0])
 			}
-			result := make([]string, len(str))
-			for key, value := range str {
-				result[key] = fmt.Sprintf("%d. %s", key+1, value)
-			}
-			out := strings.Join(result, "<br/>")
+			//result := make([]string, len(str))
+			//for key, value := range str {
+			//result[key] = fmt.Sprintf("%d. %s", key+1, value)
+			//}
+			out := strings.Join(str, "<br/>")
 			return template.HTML(out)
 		},
 	}).Parse(readme))
